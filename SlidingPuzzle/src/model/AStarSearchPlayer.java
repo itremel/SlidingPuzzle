8c88d98d3c0e3bcb3b58d6044dd384ecb32506ec
Lennard <lennardnoehren@gmx.de>
2017-05-24 11:01:32 +0200

    Einige Sachen gefixt, funktioniert jetzt, Warnings entfernt

--- SlidingPuzzle/src/model/AStarSearchPlayer.java
+++ SlidingPuzzle/src/model/AStarSearchPlayer.java
@@ -1,7 +1,6 @@
 package model;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
@@ -13,20 +12,21 @@ public class AStarSearchPlayer extends Player{
 
 	class Node{
 		Integer[][] board;
+		int steps = 0;
 		int heuristic = 0;
 		Node parent = null;
-		action direction = null;
+		action previousDirection = null;
 		
-		Node(Integer[][] board2, int heuristic2, Node parent2, action dir){
-			board = board2;
-			heuristic = heuristic2;
-			parent = parent2;
-			direction = dir;
+		Node(Integer[][] board, int steps, int heuristic, Node parent, action dir){
+			this.board = board;
+			this.steps = steps;
+			this.heuristic = heuristic;
+			this.parent = parent;
+			this.previousDirection = dir;
 		}
 		
 		@Override
 		public boolean equals(Object arg0) {
-			// TODO Auto-generated method stub
 			if (arg0 == this) return true;
 			if(!(arg0 instanceof Node)){
 				return false;
@@ -37,46 +37,33 @@ public class AStarSearchPlayer extends Player{
 		
 		@Override
 		public int hashCode() {
-			// TODO Auto-generated method stub
-			return Objects.hash(board);
+			return Objects.hash((Object)board);
 		}
 	}
 	
 	@Override
 	public List<action> solve(PuzzleGame game) {
 		List<action> result = new ArrayList<>();
-		/**
-		Integer[][] currentBoard = game.getGameBoard();
-		for (int i=0; i<20; ++i) {
-			action[] allowedActions = game.getPossibleActions(currentBoard);
-			int nextAction =(int)Math.ceil(Math.random()* allowedActions.length)-1;			
-			action next = allowedActions[nextAction];
-			currentBoard = game.computeAction(next, currentBoard);
-			result.add(next);
-		}
-		**/
 		List<Node> open = new ArrayList<>();
 		List<Node> closed = new ArrayList<>();
 		
 		Integer[][] currentBoard = game.getGameBoard();
 		int heuristic = game.getHeuristicValue(currentBoard);
-		Node root = new Node(currentBoard, heuristic, null, null);
+		Node root = new Node(currentBoard, 0, heuristic, null, null);
 		open.add(root);
 		
 		while(!open.isEmpty()){
 			
 			//best value in open
 			order(open);
-//			System.out.println("open: "+open.size());
 			Node curNode = open.get(0);
 			open.remove(0);
 			closed.add(curNode);
-			System.out.println("closed: "+closed.size());
 			//check final state
 			if(game.isSolution(curNode.board)){
 				System.out.println("solution found");
-				while(curNode!=null){
-					result.add(curNode.direction);
+				while(curNode.parent!=null){
+					result.add(curNode.previousDirection);
 					curNode = curNode.parent;
 				}
 				Collections.reverse(result);
@@ -94,20 +81,19 @@ public class AStarSearchPlayer extends Player{
 	
 	private void expand(PuzzleGame game ,Node curNode, List<Node> open, List<Node> closed) {
 		//expand successors
-		List<Node> neighbours = new ArrayList<>();
 		
 		action[] allowedActions = game.getPossibleActions(curNode.board);
 		for (int i = 0; i < allowedActions.length; i++) {
 			action next = allowedActions[i];
 			Integer[][] b = game.computeAction(next, curNode.board);
 			int h = game.getHeuristicValue(b);
-			Node successor = new Node(b, h, curNode, next);
-			if(!Arrays.asList(closed).contains(successor)){//node not in closed
+			Node successor = new Node(b, curNode.steps + 1, h, curNode, next);
+			if(!(closed).contains(successor)){//node not in closed
 				open.add(successor);
 //				System.out.println("add to open");
 			}else{
-				System.out.println("contained in closed");
-//				 Sonst: Aktualisiere gemerkten Elternknoten des Nachfolgers, wenn neuer Weg über n kostengünstiger ist als vorheriger.
+//				System.out.println("contained in closed");
+//				 Sonst: Aktualisiere gemerkten Elternknoten des Nachfolgers, wenn neuer Weg ï¿½ber n kostengï¿½nstiger ist als vorheriger.
 //				if(successor.heuristic<closed.)
 //				neighbours.get(i).parent
 			}
@@ -115,14 +101,13 @@ public class AStarSearchPlayer extends Player{
 	}
 
 	private static void order(List<Node> nodes){
-		Collections.sort(nodes, new Comparator() {
+		Collections.sort(nodes, new Comparator<Node>() {
 
 			@Override
-			public int compare(Object arg0, Object arg1) {
-				// TODO Auto-generated method stub
-				Integer heuristic1 = ((Node)arg0).heuristic;
-				Integer heuristic2 = ((Node)arg1).heuristic;
-				return heuristic1.compareTo(heuristic2);
+			public int compare(Node arg0, Node arg1) {
+				Integer value1 = arg0.heuristic + arg0.steps;
+				Integer value2 = arg1.heuristic + arg1.steps;
+				return value1.compareTo(value2);
 			}
 			
 		});
